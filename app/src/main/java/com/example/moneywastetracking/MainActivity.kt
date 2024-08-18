9568d4dd755a0b25269d48474b32a0fcf5e8255b
Jason Wan <njasow@g.ucla.edu>
2024-08-18 12:18:48 -0700

    Finished main page v1

--- app/src/main/java/com/example/moneywastetracking/MainActivity.kt
+++ app/src/main/java/com/example/moneywastetracking/MainActivity.kt
@@ -16,22 +16,45 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.moneywastetracking.ui.theme.MoneyWasteTrackingTheme
 
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+
+import java.math.BigDecimal
+import java.math.RoundingMode
+// TODO
+/*
+    Hourly Rate Input:
+    - Pressing Enter should automatically save the input, not go to new line
+    - Needs to be a reset button that returns the hourly rate to an input-able state,
+        and sets hourly rate back to 0
+    - The reset button will be the same as the start overlay service button after the start
+        button has been clicked.
+
+    - Total page:
+        - Total time wasted, Today, past week, past month, past 3 months, past 6 months, past year
+        - Total money wasted ---------------------
+    - Changing your hourly rate doesn't necessarily reset the counter, you will have the option to
+        change your rate without reseting the counter by default, and resetting the counter
+        to zero if you want.
+    - Total Money Wasted has to be an accumulation of all sessions and different hourly rates.
+ */
+
 
 class MainActivity : ComponentActivity() {
-    private var hourlyRate: Float = 0f
-//    private var totalTime: Float = 0f
-    private var time by mutableStateOf(0f)
+    private var hourlyRate by mutableStateOf(0f)
+    private var hours by mutableStateOf(0)
+    private var minutes by mutableStateOf(0)
+    private var seconds by mutableStateOf(0)
+    private var moneywasted by mutableStateOf(0f)
     private var job: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,40 +65,43 @@ class MainActivity : ComponentActivity() {
                     MainContent(
                         modifier = Modifier.padding(innerPadding),
                         onStartOverlayService = {
-//                            startOverlayService()
                             startTimer()
                         },
                         onHourlyRateChange = { rate ->
                             hourlyRate = rate
                         },
-//                        totalTime = totalTime,
-                        time = time,
-                        onResetCurrent= {
-                            time = 0f
+                        hours = hours,
+                        minutes = minutes,
+                        seconds = seconds,
+                        onResetCurrent = {
+                            hours = 0
+                            minutes = 0
+                            seconds = 0
+                            moneywasted = 0f
                             job?.cancel()
-                        }
+                        },
+                        moneywasted = moneywasted,
                     )
                 }
             }
         }
     }
 
-    private fun startOverlayService() {
-        val intent = Intent(this, OverlayService::class.java).apply {
-            putExtra("HOURLY_RATE", hourlyRate)
-//            putExtra("TOTAL_TIME", totalTime)
-            putExtra("TIME", time)
-        }
-        startService(intent)
-    }
-
     private fun startTimer() {
+        job?.cancel()
         job = lifecycleScope.launch {
             while (true) {
-//                delay(360000) // 6 minutes in milliseconds
-                delay(1000) // 6 minutes in milliseconds
-                time += 6
-//                totalTime += 6
+                delay(1000) // 1 second in milliseconds
+                seconds++
+                if (seconds == 60) {
+                    seconds = 0
+                    minutes++
+                    if (minutes == 60) {
+                        minutes = 0
+                        hours++
+                    }
+                }
+                moneywasted = BigDecimal(hourlyRate * (hours + minutes / 60.0 + seconds / 3600.0).toDouble()).setScale(3, RoundingMode.HALF_UP).toFloat()
             }
         }
     }
@@ -86,30 +112,15 @@ fun MainContent(
     modifier: Modifier = Modifier,
     onStartOverlayService: () -> Unit,
     onHourlyRateChange: (Float) -> Unit,
-//    totalTime: Float,
-    time: Float,
+    hours: Int,
+    minutes: Int,
+    seconds: Int,
     onResetCurrent: () -> Unit,
+    moneywasted: Float,
 ) {
     var hourlyRateInput by remember { mutableStateOf("") }
-
-
-    // TODO
-    /*
-        Hourly Rate Input:
-        - Pressing Enter should automatically save the input, not go to new line
-        - Needs to be a reset button that returns the hourly rate to an input-able state,
-            and sets hourly rate back to 0
-        - The reset button will be the same as the start overlay service button after the start
-            button has been clicked.
-
-        - Total page:
-            - Total time wasted, Today, past week, past month, past 3 months, past 6 months, past year
-            - Total money wasted ---------------------
-        - Changing your hourly rate doesn't necessarily reset the counter, you will have the option to
-            change your rate without reseting the counter by default, and resetting the counter
-            to zero if you want.
-        - Total Money Wasted has to be an accumulation of all sessions and different hourly rates.
-     */
+    val context = LocalContext.current
+    val focusManager = LocalFocusManager.current
 
     Column(modifier = modifier.padding(16.dp)) {
         Text(text = "Money Waste Tracker", style = MaterialTheme.typography.headlineMedium)
@@ -120,7 +131,11 @@ fun MainContent(
             onValueChange = {
                 hourlyRateInput = it
                 it.toFloatOrNull()?.let { rate ->
-                    onHourlyRateChange(rate)
+                    if (rate >= 0) {
+                        onHourlyRateChange(rate)
+                    } else {
+                        onHourlyRateChange(0f)
+                    }
                 }
             },
             label = { Text("Hourly Rate $/hr") },
@@ -136,28 +151,21 @@ fun MainContent(
         ) {
             Text(text = "Start Overlay Service")
         }
+        Text(text = "Time Wasted: $hours hr $minutes min $seconds s")
+        Text(text = "Current Hourly Rate: $$hourlyRateInput per hour")
+        Text(text = "Money Wasted in Current Session: $${moneywasted}")
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text(text = "Time Wasted in Current Session: $time hours")
-//        Text(text = "Total Money Wasted: ${totalTime * hourlyRate} dollars")
+//        Text(text = "Time Wasted in Current Session: $time hours")
 
         Button(
             onClick = onResetCurrent,
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text(text = "Reset Current Session")
+            Text(text = "Stop and Reset Current Session")
         }
-//        Spacer(modifier = Modifier.height(16.dp))
-//        Text(text = "Total Time Wasted: $totalTime hours")
-//        Button(
-//            onClick = onResetTotal,
-//            modifier = Modifier.fillMaxWidth()
-//        ) {
-//            Text(text = "Reset Total")
-//        }
     }
-
 }
 
 @Preview(showBackground = true)
@@ -167,11 +175,11 @@ fun MainContentPreview() {
         MainContent(
             onStartOverlayService = {},
             onHourlyRateChange = {},
-//            totalTime = 0f,
-            time = 0f,
+            hours = 0,
+            minutes = 0,
+            seconds = 0,
             onResetCurrent = {},
-//            onResetTotal = {}
+            moneywasted = 0f,
         )
     }
 }
-
